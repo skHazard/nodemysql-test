81307cdcb696dbd267937e1ca030d56701c87d01
skHazard <hazard.sk@gmail.com>
2017-06-13 12:13:38 +0300

    adding db connection

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 /**
  * Created by Hazard on 13.06.2017.
  */
+var express = require('express');
+var app = express();
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '1234',
+    database : 'inventory'
+});
+
+connection.connect(function(err){
+    if(!err) {
+        console.log("Database connected successfully... \n\n");
+    } else {
+        console.log("Error connecting to database ... \n\n");
+    }
+});
+
+
+
+app.get('/', function (req, res) {
+    res.send('Testing app is running!')
+});
+
+app.listen(3000, function () {
+    console.log('May the Node be with you on port 3000! :)')
+});
+
+
+
