eba1374e74954d8c1cac4b6a279b56431dab7d44
skHazard <hazard.sk@gmail.com>
2017-06-14 16:26:02 +0300

    adding ejs template and bootstrap

--- app.js
+++ app.js
@@ -5,20 +5,35 @@ var routes = require('./routes/router');
 var express = require('express');
 
 var app = express();
+//
+// app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
+app.use(express.static(__dirname + '/'));
 routes(app);
 
-
-app.use(function (err, req, res) {
-    res.sendStatus(500);
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
 
 
 app.listen(3000, function () {
--- routes/router.js
+++ routes/router.js
@@ -8,9 +8,9 @@ var path = require('path');
 
 module.exports = function (app) {
 
-    app.get('/', function (req, res) {
-        res.sendFile('index.html', { root: path.join(__dirname, '../') });
-    });
+    // app.get('/', function (req, res) {
+    //     res.sendFile('index.html', { root: path.join(__dirname, '../') });
+    // });
 
     app.get('/:id?', function (req, res, next) {
 
